6c8793e6cb4492a998f25462c6fb098a53d1eed6
Emil Karamihov <e.karamihov@student.fontys.nl>
2017-04-02 09:50:37 +0200

    php

---
+++ PHP/PHP/LogOut.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_['logOut'])) {
+    session_start();
+    session_destroy();
+    unset($_SESSION['username']);
+    $_SERVER['message'] = "You are logged out";
+    header("location: index.php");
+    die();
+}
+?>
